b9db359e1c76b742898faa08fa22b916b6bc850f
HimanshuKSingh <singhhks280504@gmail.com>
2026-02-04 07:41:35 +0530

    Missing number Program

---
+++ Bit_Manipulation/Missingnumber.java
@@ -0,0 +1,18 @@
+package Bit_Manipulation;
+public class Missingnumber {
+    class Solution {
+    public int missingNumber(int[] nums) {
+        int xor = 0;
+        int n = nums.length;
+
+        for (int i = 0; i < n; i++) {
+            xor ^= i;
+            xor ^= nums[i];
+        }
+
+        xor ^= n;
+
+        return xor;
+    }
+}
+}
